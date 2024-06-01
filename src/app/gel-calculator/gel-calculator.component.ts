d3404077e837913b5d308a9b6311acc80dc3ff22
Vladimir Kosickij <vladimir.kosickij@gmail.com>
2024-06-01 22:35:00 +0300

    Table works perfectly

--- src/app/gel-calculator/gel-calculator.component.ts
+++ src/app/gel-calculator/gel-calculator.component.ts
@@ -1,5 +1,4 @@
 import { Component, Pipe } from '@angular/core';
-import { MatrixTableComponent } from '../matrix-table/matrix-table.component';
 import { CommonModule } from '@angular/common';
 import { Matrix } from '../_types/matrix';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-gel-calculator',
   standalone: true,
-  imports: [MatrixTableComponent, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './gel-calculator.component.html',
   styleUrl: './gel-calculator.component.scss'
 })
@@ -15,10 +14,11 @@ export class GelCalculatorComponent {
   order = 3;
   matrix = new Matrix(this.order, this.order + 1);
 
-  onMatrixChange(newMatrix: Matrix): void {
-    this.matrix = newMatrix;
-    console.log('Matrix changed:', this.matrix);
+  resizeMatrix(rows: number, cols: number): void {
+    this.matrix.resize(rows, cols);
   }
 
-
+  trackByFn(index: any, item: any) {
+    return index
+  }
 }
